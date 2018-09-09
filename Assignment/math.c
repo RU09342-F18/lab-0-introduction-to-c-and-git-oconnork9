3c8358beb258fcd5ac8f9eb0714e63e60022f0a4
Kieran O'Connor <35498028+oconnork9@users.noreply.github.com>
2018-09-09 12:23:17 -0400

    Update math.c

--- Assignment/math.c
+++ Assignment/math.c
@@ -5,17 +5,27 @@ int main(void)
 {
     int math(int num1, int num2, char Operator);
     switch(Operator) {
-      case '+' : return (num1+num2);
-      case '-' : return (num1-num2);
-      case '*' : return (num1*num2);
-      case '/' : return (num1/num2);
-      case '%' : return (num1%num2);
-      case '<' : return (num1<num2);
-      case '>' : return (num1>num2);
-      case '&' : return (num1&num2);
-      case '|' : return (num1|num2);
-      case '^' : return (num1^num2);
-      case '~' : return (~num1);
-    
+      case '+' : return (num1+num2); //Addition
+	break;
+      case '-' : return (num1-num2); //Subtraction
+	break;
+      case '*' : return (num1*num2); //Multiplication
+      	break;
+      case '/' : return (num1/num2); //Division
+      	break;
+      case '%' : return (num1%num2); //Remainder
+     	break;
+      case '<' : return (num1<num2); //Shift Left
+      	break;
+      case '>' : return (num1>num2); //Shift Right
+      	break;
+      case '&' : return (num1&num2); //Bitwise AND
+      	break;
+      case '|' : return (num1|num2); //Bitwise OR
+      	break;
+      case '^' : return (num1^num2); //Bitwise XOR
+      	break;
+      case '~' : return (~num1);     //Inverse
+    	
 	return 0;
 }
